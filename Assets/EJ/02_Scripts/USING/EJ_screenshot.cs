19dc78e2db56b0bdf1818022767f58aa48038746
DORACHO <135581648+DORACHO@users.noreply.github.com>
2023-12-08 16:31:48 +0900

    screenshot PC버전까지 완료

--- Assets/EJ/02_Scripts/USING/EJ_screenshot.cs
+++ Assets/EJ/02_Scripts/USING/EJ_screenshot.cs
@@ -1,18 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EJ_screenshot : MonoBehaviour
 {
+    public GameObject screenShot;
+    public RawImage screenshotIMG;
+    public RawImage defaultIMG;
+
+    private string lastSavedFilePath;
     public void ClickScreenShot()
     {
-        string timeStamp = System.DateTime.Now.ToString("yyyy-MM-dd");
+        string timeStamp = System.DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss");
         string fileName = "SongCamp-ScreenShot-" + timeStamp + ".png";
 
+        // 바탕화면에 저장
+        string desktopPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.DesktopDirectory);
+        string filePath = Path.Combine(desktopPath, fileName);
 
-        //ScreenCapture.CaptureScreenshot("SongCamp.PNG");
-        ScreenCapture.CaptureScreenshot("~/Downloads/" + fileName);
+        lastSavedFilePath = filePath;
+        ScreenCapture.CaptureScreenshot(filePath);
+        Debug.Log("스크린샷이 저장된 경로: " + filePath);
 
-        print("스크린캡처가 실행되었습니다");
+
+        // 방금 저장한 이미지 표시
+        // 코루틴을 사용하여 스크린샷 저장 후 처리
+        StartCoroutine(WaitForScreenshot());
+
+    }
+    IEnumerator WaitForScreenshot()
+    {
+       
+        yield return new WaitUntil(() => File.Exists(lastSavedFilePath));
+
+        screenShot.SetActive(true);
+        // 스크린샷을 Texture2D로 변환하여 RawImage에 표시
+        byte[] fileData = File.ReadAllBytes(lastSavedFilePath);
+        Texture2D texture = new Texture2D(2, 2);
+        texture.LoadImage(fileData);
+        screenshotIMG.texture = texture;
+
+        // 이미지 끄기
+        StartCoroutine(ScreenshotOFF());
+    }
+
+
+    IEnumerator ScreenshotOFF()
+    {
+        yield return new WaitForSeconds(4f);
+        screenShot.SetActive(false);
     }
 }
